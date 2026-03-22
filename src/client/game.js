6f988d27373bb0c0907d40bd35c1b29563b488a7
FLKN <frederik@norregaard.info>
2026-03-22 14:32:42 +0100

    blab

--- src/client/game.js
+++ src/client/game.js
@@ -1,75 +1,46 @@
 console.log('Here we go!');
 document.addEventListener("DOMContentLoaded", loadPage);
-
-type KeyboardLetter = {
-    char: string,
-    isInWord: boolean,
-    isCorrect: boolean
-}
-
-type Word = {
-    letter1: KeyboardLetter,
-    letter2: KeyboardLetter,
-    letter3: KeyboardLetter,
-    letter4: KeyboardLetter,
-    letter5: KeyboardLetter,
-    isGuessed: boolean,
-    isCorrect: boolean
-}
-
 function loadPage() {
     document.addEventListener('keydown', typeLetter);
+    // document.addEventListener('click', updateVariables); // Hver gang der klikkes dobbelttjekker vi at bokse er opdateret korrekt, saa man ogsaa kan bruge skaerm tastaturet
 }
-
-function updateVariables(): void {
+function updateVariables() {
     wordContainer = wordContainers[currentGuess];
     currentCharBox = wordContainer.children[currentChar];
 }
-
-const enum keys { BACKSPACE = 8, ENTER = 13, }
-
-const wordContainers: HTMLCollection = document.getElementsByClassName("wordContainer");
-let currentChar: number = 0;
-let currentGuess: number = 0;
-let wordContainer: any = wordContainers[currentGuess];
-let currentCharBox: any = wordContainer.children[currentChar];
-
-function typeLetter(event: KeyboardEvent): void {
-    const ascii_key: number = event.keyCode;
-    const char: string = String.fromCharCode(ascii_key);
-
-    if (ascii_key == keys.BACKSPACE) {
+var wordContainers = document.getElementsByClassName("wordContainer");
+var currentChar = 0;
+var currentGuess = 0;
+var wordContainer = wordContainers[currentGuess];
+var currentCharBox = wordContainer.children[currentChar];
+function typeLetter(event) {
+    var ascii_key = event.keyCode;
+    var char = String.fromCharCode(ascii_key);
+    if (ascii_key == 8 /* keys.BACKSPACE */) {
         console.log("Backspace!");
         deleteLetter();
-
-    } else if (ascii_key == keys.ENTER) {
+    }
+    else if (ascii_key == 13 /* keys.ENTER */) {
         console.log('Enter!');
         guessWord();
-
-    } else if (isLetter(char) && currentChar <= 5) {
+    }
+    else if (isLetter(char) && currentChar <= 5) {
         putLetter(char);
-
-    } else {
+    }
+    else {
         console.log('Invalid key');
     }
 }
-
-
-function guessWord(): void {
-
+function guessWord() {
     updateVariables();
-
     if (currentChar != 5) {
-        console.log('Gotta type all 5 letters to guess a word!')
+        console.log('Gotta type all 5 letters to guess a word!');
         return;
     }
-
-    let guess: string = "";
-
-    for (let i: number = 0; i < wordContainer.children.length; i++) {
+    var guess = "";
+    for (var i = 0; i < wordContainer.children.length; i++) {
         guess = guess + wordContainer.children[i].textContent;
     }
-
     // for (word in valid_words) {
     //     if (guess == word) {
     //         // TODO: Send word til server for at gaette. guessWord() skal maaske vare async
@@ -80,77 +51,58 @@ function guessWord(): void {
     //         return;
     //     }
     // }
-
     // console.log('Not valid guess');
-
     console.log('Guessed word: ' + guess);
     currentGuess++;
     currentChar = 0;
 }
-
 // Hvis vi trykker backspace saa sletter vi bogstavet boksen, hvis boksen er tom, gaar vi lige en boks tilbage for at slette.
-function deleteLetter(): void {
-
+function deleteLetter() {
     updateVariables();
-
     console.log('deleting letter');
-
     if (currentChar == 5) {
         currentChar--;
         currentCharBox = wordContainer.children[currentChar];
     }
-
     if (wordContainer != undefined && currentCharBox != undefined) {
-
-        console.log('current textcontent: ' + currentCharBox.textContent)
-
+        console.log('current textcontent: ' + currentCharBox.textContent);
         if (currentCharBox.textContent == "" && currentChar > 0) {
             currentChar--;
             currentCharBox = wordContainer.children[currentChar];
         }
-
         currentCharBox.textContent = "";
-
         if (currentChar > 0) {
             currentChar--;
         }
-
         console.log("Current char num: " + currentChar);
     }
 }
-
-function putLetter(char: string): void {
-
+function putLetter(char) {
     updateVariables();
-
     if (wordContainer != undefined && currentCharBox != undefined) {
-
         if (currentCharBox.textContent != "" && currentChar >= 0 && currentChar <= 4) {
             currentChar++;
             currentCharBox = wordContainer.children[currentChar];
             currentCharBox.textContent = char;
-
-        } else if (currentCharBox.textContent == "" && currentChar >= 0 && currentChar <= 4) {
+        }
+        else if (currentCharBox.textContent == "" && currentChar >= 0 && currentChar <= 4) {
             currentCharBox.textContent = char;
             currentChar++;
         }
-
         console.log("Current char num: " + currentChar);
     }
-
     else {
         console.log('Can\'t type this');
     }
 }
-
 // Source - https://stackoverflow.com/a/9862788
 // Posted by JaredPar
 // Retrieved 2026-03-21, License - CC BY-SA 3.0
-function isLetter(str: string): boolean {
+function isLetter(str) {
     if (str.length === 1 && str.match(/[a-z]/i)) {
         return true;
-    } else {
+    }
+    else {
         return false;
     }
 }
-
