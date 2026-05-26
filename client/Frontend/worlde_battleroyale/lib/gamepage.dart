9312238e930bba17f2518c6384bb90a7c49025f0
SorenL <sorenlasskogen@gmail.com>
2026-05-27 00:37:26 +0200

    Begyndt at splitte states i scripts

---
+++ client/Frontend/worlde_battleroyale/lib/gamepage.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 500),
+      curve: Curves.bounceIn,
+      height: 60,
+      width: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white,
+        },
+      ),
+      child: Center(
+        child: Text(
+          letter.toUpperCase(),
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+    );
+  }
+}
+
+class GamePage extends StatefulWidget {
+  GamePage({super.key});
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          for (var guess in _game.guesses)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (var letter in guess)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 2.5, vertical: 2.5),
+                    child: Tile(letter.char, letter.type),
+                  )
+              ],
+            ),
+          GuessInput(
+           onSubmitGuess: (String guess) {
+              setState(() { // NEW
+                _game.guess(guess);
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- client/Frontend/worlde_battleroyale/lib/main.dart
+++ client/Frontend/worlde_battleroyale/lib/main.dart
@@ -1,12 +1,15 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
+
+// siger ret meget sig selv..
 enum PlayerState { lobby, playing, spectating }
 
+
+// Hvad der styrer hvilken skærm der vises, self afhænging af state
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
@@ -14,6 +17,9 @@ class MainApp extends StatefulWidget {
   State<MainApp> createState() => _MainAppState();
 }
 
+
+// Det virker PT, skal nok ændres lidt
+// TO DO => opdel alle states i nye filer, og importer der her, så det ikke bliver alt for rodet
 class _MainAppState extends State<MainApp> {
   PlayerState _playerState = PlayerState.lobby;
   void _setPlayerState(PlayerState newState) => setState(() => _playerState = newState);
@@ -33,10 +39,8 @@ class _MainAppState extends State<MainApp> {
       case PlayerState.spectating:
         body = const Text('Spectating Screen');
         break;
-      default:
-        body = LobbyScreen(onPlayerStateChange: _setPlayerState);
     }
-
+// Hvis bare appbar og body igennem alle states
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Wordle Battle Royale')),
