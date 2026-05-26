6b74d3f7f3a29bef5478b83a01c96bb1b7239368
SorenL <sorenlasskogen@gmail.com>
2026-05-26 17:06:54 +0200

    States implemented

--- client/Frontend/worlde_battleroyale/lib/main.dart
+++ client/Frontend/worlde_battleroyale/lib/main.dart
@@ -5,23 +5,61 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+enum PlayerState { lobby, playing, spectating }
+
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  PlayerState _playerState = PlayerState.lobby;
+  void _setPlayerState(PlayerState newState) => setState(() => _playerState = newState);
+
+
+  @override
   Widget build(BuildContext context) {
+    Widget body; 
+
+    switch (_playerState) {
+      case PlayerState.lobby:
+        body = LobbyScreen(onPlayerStateChange: _setPlayerState);
+        break;
+      case PlayerState.playing:
+        body = const Text('Playing Screen');
+        break;
+      case PlayerState.spectating:
+        body = const Text('Spectating Screen');
+        break;
+      default:
+        body = LobbyScreen(onPlayerStateChange: _setPlayerState);
+    }
+
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Align(
-            alignment: Alignment.center,
-            child: Text('Home Page Demo', 
-            style: TextStyle(fontSize: 50,fontWeight: FontWeight.bold, 
-            ),
-            ),
-          )
-        ),
+        appBar: AppBar(title: const Text('Wordle Battle Royale')),
+        body: Center(child: body),
       ),
     );
+  }
+}
+
+class LobbyScreen extends StatelessWidget {
+  final void Function(PlayerState) onPlayerStateChange;
+
+  const LobbyScreen({super.key, required this.onPlayerStateChange});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisSize: MainAxisSize.min, children: [
+      const Text ('Lobby', style: TextStyle(fontSize: 28)),
+      const SizedBox(height:12),
+      ElevatedButton(
+        onPressed: () => onPlayerStateChange(PlayerState.playing),
+        child: const Text('Start Game'),
+      )
+    ]);
   }
 }
