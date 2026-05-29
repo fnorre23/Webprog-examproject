1b54e4cae30e3339aab8954a9b8713216dde5084
SorenL <sorenlasskogen@gmail.com>
2026-05-29 14:23:13 +0200

    Data til backend logik
    Arbejder på at sende data til backend, samt implementere logik der behandler data fra backend

--- client/Frontend/worlde_battleroyale/lib/playingScreen.dart
+++ client/Frontend/worlde_battleroyale/lib/playingScreen.dart
@@ -1,9 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
 
 // Skal i back-end
 enum HitType { hit, partial, miss }
 
-// Alt det her er fra FLutter Tutorial https://docs.flutter.dev/learn/pathway/tutorial
+
+//Behandler json fra backend
+class LetterInfo {
+  final String char;
+  final HitType type;
+
+  LetterInfo({required this.char, required this.type});
+
+  factory LetterInfo.fromJson(Map<String, dynamic> json) {
+    return LetterInfo(
+      char: json['char'],
+      type: HitType.values.byName(json['type'])
+    );
+  }
+}
+
+
+// sender guess til backend
+// TO-do => skal også kunne behandle response fra backend, og opdatere guesses
+class Game {
+  final socket =io.io('http://localhost:8080', io.OptionBuilder()
+    .setTransports(['websocket'])
+    .disableAutoConnect()
+    .build());
+
+  final void Function()? onUpdate;
+  List<List<LetterInfo>> guesses = [];
+
+  Game({this.onUpdate}) {
+    socket.connect();
+    socket.on('response', (data) {
+      print ('Recived response: $data');});
+  }
+
+  void guess(String guess) {
+    socket.emit('guess', guess);
+    print('Guess sent: $guess');
+  }
+}
+
+
+// Det meste af det her er fra FLutter Tutorial https://docs.flutter.dev/learn/pathway/tutorial
 
 class Tile extends StatelessWidget {
   const Tile(this.letter, this.hitType, {super.key});
@@ -38,14 +80,20 @@ class Tile extends StatelessWidget {
 }
 
 class GamePage extends StatefulWidget {
-  GamePage({super.key});
+  const GamePage({super.key});
 
   @override
   State<GamePage> createState() => _GamePageState();
 }
 
 class _GamePageState extends State<GamePage> {
-  final Game _game = Game();
+  late final Game _game;
+
+  @override
+  void initState() {
+    super.initState();
+    _game = Game(onUpdate: () => setState(() {}));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,13 +114,65 @@ class _GamePageState extends State<GamePage> {
             ),
           GuessInput(
            onSubmitGuess: (String guess) {
-              setState(() { // NEW
                 _game.guess(guess);
-              });
             },
           ),
         ],
       ),
     );
   }
+}
+
+class GuessInput extends StatelessWidget {
+  GuessInput({super.key, required this.onSubmitGuess});
+
+  final void Function(String) onSubmitGuess;
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+  final FocusNode _focusNode = FocusNode();
+
+  void _onSubmit() {
+    onSubmitGuess(_textEditingController.text);
+    _textEditingController.clear();
+    _focusNode.requestFocus();
+  }
+
+@override
+Widget build(BuildContext context) {
+  const int maxRows = 6;
+
+  final boardRows = List.generate(maxRows, (rowIndex) {
+    if (rowIndex < _game.guesses.length) {
+      return _game.guesses[rowIndex];
+    }
+    return List.generate(
+      5,
+      (_) => LetterInfo(char: '', type: HitType.miss),
+    );
+  });
+
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      children: [
+        for (var guess in boardRows)
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (var letter in guess)
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 2.5, vertical: 2.5),
+                  child: Tile(letter.char, letter.type),
+                ),
+            ],
+          ),
+        GuessInput(
+          onSubmitGuess: (String guess) {
+            _game.guess(guess);
+          },
+        ),
+      ],
+    ),
+  );
 }
